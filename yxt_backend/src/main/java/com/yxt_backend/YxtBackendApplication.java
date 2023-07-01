923e10a9e933491a3c247c8bb9be9c2cca9702ca
ryl <renyouliang69@163.com>
2023-07-01 19:02:11 +0800

    start

---
+++ yxt_backend/src/main/java/com/yxt_backend/YxtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yxt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YxtBackendApplication.class, args);
+    }
+
+}
